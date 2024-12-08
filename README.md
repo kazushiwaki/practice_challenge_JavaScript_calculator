0a2ba8ff3a00d598a7a771d7d45389da8358d4e3
rq-kazushiwaki-at-321899771753 <7zcorgizyonna1144@gmail.com>
2024-12-08 11:19:45 +0900

    chore:最初のコミット index.html, main.css, main.jsファイルを新規追加しました。また、ファイル間の読み込みを確認し、正常に読み込みが出来ておりました。

---
+++ README.md
@@ -0,0 +1,5 @@
+## 概要
+＜実践課題＞Javascript 電卓
+
+## 内容
+JavaScriptを利用して、電卓の作成。
---
+++ main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $("h3").mouseover(function(){
+    $(this).css("color", "black");
+    console.log("マウスが文字に乗りました");
+  });
+});
